3d509a4ec2b141bd2bda1dc3e9071b8b5b029c56
JesusMaturino <jesus.maturino.isw@gmail.com>
2022-01-29 16:36:42 -0600

    update

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -10,7 +10,7 @@ class Inicio extends Component {
       message: '',
       message2: '',
       message3: '',
-      array: []
+      array: [],
     }
     this.Registrar = this.Registrar.bind(this);
     this.Salida = this.Salida.bind(this);
@@ -33,7 +33,7 @@ class Inicio extends Component {
     window.location.reload(true);
   }
   
-  async Registrar() {
+  Registrar() {
 
     var numero_placa = document.getElementById("placa").value;
     var tipo = document.getElementById("tipo").value;
@@ -56,20 +56,26 @@ class Inicio extends Component {
         mes = "0"+mes;
     }
 
+    if (minuto < 10) {
+        minuto = "0"+minuto;
+    }
+
+
     let fecha_completa = ""+year+"-"+mes+"-"+dia+"T"+hora+":"+minuto+":00.000Z";
+
+    numero_placa = numero_placa.toUpperCase();
 
     var payload={
         "numero_placa": numero_placa,
         "tiempo_entrada": fecha_completa,
-        "tiempo_salida": fecha_completa,
+        "tiempo_salida": "",
         "tipo": tipo
     }
 
-    console.log(payload);
 
-        axios.post('http://localhost:8000', payload)
+        axios.post('http://localhost:8000/estacionamiento', payload)
         .then((response) => {
-            
+          // console.log(response);
             if(response.status === 200){
 
                 var message = "Guardado correctamente.";
@@ -85,12 +91,13 @@ class Inicio extends Component {
     }
     }
 
-    async Salida() {
+    Salida() {
 
         var numero_placa = document.getElementById("placaSalida").value;
+        var hora_salida = document.getElementById("horaSalida").value;
 
-        if (numero_placa === ''){
-            var message2 = "Completa el campo.";
+        if (numero_placa === '' || hora_salida === ''){
+            var message2 = "Completa todos los campos.";
             this.setState({message2:message2});
             setTimeout(this.Clean,3000);
         }else{
@@ -100,25 +107,23 @@ class Inicio extends Component {
         let dia = date.getDate()
         let mes = date.getMonth() + 1
         let year = date.getFullYear()
-        let hora = date.getHours()
-        let minuto = date.getMinutes()
 
         if (mes < 10) {
             mes = "0"+mes;
         }
 
-        let fecha_completa = ""+year+"-"+mes+"-"+dia+"T"+hora+":"+minuto+":00.000Z";
-        
+        let fecha_completa = ""+year+"-"+mes+"-"+dia+"T"+hora_salida+":00.000Z";
+        numero_placa = numero_placa.toUpperCase();
+
         var payload={
             "numero_placa": numero_placa,
             "tiempo_salida": fecha_completa
         }
     
-        console.log(payload);
     
-            axios.put('http://localhost:8000', payload)
+            axios.put('http://localhost:8000/estacionamiento', payload)
             .then((response) => {
-                
+              // console.log(response);
                 if(response.status === 200){
     
                     var message2 = "Guardado correctamente.";
@@ -130,14 +135,19 @@ class Inicio extends Component {
     
             .catch((error) => {
                 console.log(error);
+                var message2 = "No se reconoce la placa.";
+                this.setState({message2:message2});
+                setTimeout(this.Clean,3000);
             });
         }
     }
 
-    async Ver() {
+    Ver() {
 
         var fecha = document.getElementById("fecha").value;
         var hora = document.getElementById("hora").value;
+
+        var horaSegmentada = hora.split(":");
 
         if (fecha === '' || hora === ''){
             var message3 = "Completa todos los campos.";
@@ -145,26 +155,31 @@ class Inicio extends Component {
             setTimeout(this.Clean,3000);
         }else{
         
-        let tiempo_entrada = ""+fecha+"T"+hora+":00.000Z";
-        console.log(tiempo_entrada)
+        let tiempo_entrada = ""+fecha+"T"+horaSegmentada[0];
 
     
-            axios.get('http://localhost:8000/'+tiempo_entrada)
+            axios.get('http://localhost:8000/estacionamiento/'+tiempo_entrada)
             .then((response) => {
-                
+                // console.log(response);
                 if(response.status === 200){
     
-                    // var array = []
+                    var array = []
 
-                    // for (const key in response.data) {
+                    if (response.data.numero_placas.length === 0) {
+                        array.push(["Sin registro","Sin registro","Sin registro","Sin registro"]);
+                    }else{
 
-                    //   arrayCompleto.push(response.data[key].idFORMULARIO)
+                      for (const key in response.data.numero_placas) {
+                        const element = response.data; 
 
-                    // }
+                        array.push([element.numero_placas[key],element.tiempo_estacionados[key],element.tipos[key],element.cantidades_a_pagar[key]])
+                      }
 
-                    // this.setState({
-                    // array: array
-                    // })
+                    }
+
+                    this.setState({
+                    array: array,
+                    })
     
                 }
             })
@@ -212,6 +227,9 @@ render() {
           <div>
             <text>Numero de Placa:   </text>
             <input type="text" label="Placa" id="placaSalida" required></input>
+            <br/><br/>
+            <text>Hora de Salida:   </text>
+            <input type="time" id="horaSalida"></input>
             <br/>
             <h5>{this.state.message2}</h5>
             <br/>
@@ -236,7 +254,7 @@ render() {
             <h5>{this.state.message3}</h5>
             <br/>
             <button onClick={this.Ver} className='btn'>Ver</button>
-            <br/><br/><br/>
+            <br/><br/>
           </div>
         </div>
 
@@ -249,24 +267,40 @@ render() {
                 <th>Cantidad a pagar</th>
             </tr>
 
-            {/* {this.state.array.map((dato, numero) => {
-                return (
-                <tr>
-                    <td>{dato[0]}</td>
-                    <td>{dato[1]}</td>
-                    <td>{dato[2]}</td>
-                    <td>{dato[3]}</td>
-                </tr>
-            )})} */}
 
-            <tr>
-                <td>AMD4</td>
-                <td>30 min</td>
-                <td>Oficial</td>
-                <td>$200.00MX</td>
-            </tr>
+              {this.state.array.map((dato, numero) => {
+
+                if (dato[2] === "noResidente") {
+                  dato[2] = "No Residente";
+                }else if(dato[2] === "residente"){
+                  dato[2] = "Residente";
+                }else if (dato[2] === "oficial") {
+                  dato[2] = "Oficial";
+                }
+
+                if (dato[3] !== "Sin registro") {
+                  dato[3] = "$ "+dato[3]+".00MX";
+                }
+
+                  return (
+                    <tr>
+                      <td>
+                        <tr>{dato[0]}</tr>
+                      </td>
+                      <td>
+                        <tr>{dato[1]}</tr>
+                      </td>
+                      <td>
+                        <tr>{dato[2]}</tr>
+                      </td>
+                      <td>
+                        <tr>{dato[3]}</tr>
+                      </td>
+                    </tr>
+              )})}
+
         </table>
-
+        <br/><br/>
       </div>
     );
   }
